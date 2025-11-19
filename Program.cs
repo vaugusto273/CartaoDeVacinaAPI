fad4ec2c8105484983171be666b8b4af309d49af
Vaugusto <samlara273@gmail.com>
2025-11-19 12:22:50 -0300

    🎉 Initial Commit

---
+++ Program.cs
@@ -0,0 +1,29 @@
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddControllers();
+
+// Register EF Core SQLite
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
---
+++ readme.md
@@ -0,0 +1,16 @@
+# CartaoDeVacinaAPI
+
+API de exemplo para cartão de vacina.
+
+Pré-requisitos
+- .NET 10 SDK
+- SQLite
+
+Como rodar
+```powershell
+dotnet restore
+dotnet build
+dotnet run
+```
+
+Swagger: /swagger/index.html
